42e2d233d8159ba85e48bb5c0ad6b3e333598e65
Cody-Price <ctprice11@gmail.com>
2019-02-05 17:08:09 -0700

    Add basic functionality

--- cobrewsapi/src/App.js
+++ cobrewsapi/src/App.js
@@ -7,34 +7,43 @@ class App extends Component {
     super();
     this.state = {
       returnedURL: 'url',
-      responseObject: 'responseObject',
       requestVerb: 'verb',
       error: '',
-      requestBody: ''
-
+      requestBody: '',
+      result: [],
+      responseStatus: ''
     }
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
     let returnedURL = ''
+    const requestBody = this.refs.textAreaSelect.value
     const requestVerb = this.refs.verbSelect.value
     const url = this.refs.urlSelect.value
     const id = this.refs.idSelect.value ? `/${this.refs.idSelect.value}` : ''
     const query = this.refs.querySelect.value ?  `?${this.refs.querySelect.value}=${this.refs.citySelect.value}` : ''
     const subTable = this.refs.subTableSelect.value ? `/beers` : ''
-    returnedURL = url + id + query + subTable
-    console.log(returnedURL)
-    this.setState({requestVerb, returnedURL, requestBody})
+    returnedURL = 'http://localhost:3000' + url + id + query + subTable
+    this.setState({error: '', result: [], requestVerb, returnedURL, requestBody, responseStatus: ''})
   }
 
   fetchEndpoint = async() => {
     try {
-      await fetch(this.state.returnedURL, {
-        method: this.state.requestVerb,
-        body: this.state.requestBody.json()
-      })
-
+      let response;
+      if (this.state.requestVerb === 'GET' || 'DELETE') {
+        response = await fetch(this.state.returnedURL, {
+          method: this.state.requestVerb
+        })
+      } else {
+        response = await fetch(this.state.returnedURL, {
+          method: this.state.requestVerb,
+          body: this.state.requestBody
+        })
+      }
+      console.log('response:', response)
+      const result = await response.json()
+      this.setState({responseStatus: response.status, result})
     } catch (error) {
       this.setState({error})
     }
@@ -65,21 +74,8 @@ class App extends Component {
                 <option value='/api/beers'>/api/beers</option>
               </select>
               <div>
-                <label>This body field must be completed for POST/PATCH/PUT of Breweries</label>
-                <input className='brew-body-input' placeholder='' name='' type='text' ref=''/>
-                <input className='brew-body-input' placeholder='' name='' type='text' ref=''/>
-                <input className='brew-body-input' placeholder='' name='' type='text' ref=''/>
-                <input className='brew-body-input' placeholder='' name='' type='text' ref=''/>
-                <input className='brew-body-input' placeholder='' name='' type='text' ref=''/>
-                <input className='brew-body-input' placeholder='' name='' type='text' ref=''/>
-              </div>
-              <div>
-                <label>This body field must be completed for POST/PATCH/PUT of Beers</label>
-                <input className='beer-body-input' placeholder='' name='' type='text' ref=''/>
-                <input className='beer-body-input' placeholder='' name='' type='text' ref=''/>
-                <input className='beer-body-input' placeholder='' name='' type='text' ref=''/>
-                <input className='beer-body-input' placeholder='' name='' type='text' ref=''/>
-                <input className='beer-body-input' placeholder='' name='' type='number' ref=''/>
+                <label>This city field is required if POST/PUT/PATCH is selected, Enter text in JSON format with required parameters.</label>
+                <textarea rows="8" columns="100" placeholder="Enter JSON formated object here" ref="textAreaSelect"></textarea>
               </div>
               <div>
                 <label>This id selector field is optional</label>
@@ -113,9 +109,16 @@ class App extends Component {
             <button onClick={this.fetchEndpoint}>Get this endpoint</button>
             <button className='resetURL'>Reselect a URL to search</button>
           </section>
-          <section>Response Example
+          <section>
+            <p className="response-status">Response Status: {this.state.responseStatus}</p>
             <div className='responseObject'>
-              {this.state.responseObject}
+              Response:
+              {this.state.result.length ? this.state.result.map(i => {
+                return <p key={i.name}>{JSON.stringify(i)}</p>
+              }) : null}
+            </div>
+            <div>
+              {this.state.error ? this.state.error.message : null}
             </div>
           </section>
         </main>
