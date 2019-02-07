756f21e490c23f6b4d0fb0104a0975ffb1d48408
Sara Ebbers <slebbers@gmail.com>
2019-02-06 17:19:28 -0700

    Build3

--- cobrewsapi/server.js
+++ cobrewsapi/server.js
@@ -4,6 +4,9 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(favicon(('./public/index.html' + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+
 app.use(express.static('./public/index.html'));
 app.use(express.static(path.join('./public/index.html', 'build')));
 app.get('/ping', function (req, res) {
